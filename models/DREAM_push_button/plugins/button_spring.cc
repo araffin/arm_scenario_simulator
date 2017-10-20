1de540687b93bb90bb49d354fe0926b0c9964872
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2017-10-20 17:35:19 +0200

    Testing button with a real spring force Need friction to avoid oscillations

--- models/DREAM_push_button/plugins/button_spring.cc
+++ models/DREAM_push_button/plugins/button_spring.cc
@@ -14,12 +14,27 @@ namespace gazebo
 
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
+    private: double k; // The spring constant
+    private: double z0;  // the equilibrium position (with no gravity)
+    private: double z_eq;  // the equilibrium position (with gravity)
 
 
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
     {
       // Store the pointer to the model
       this->model = _parent;
+      if (_sdf->HasElement("k"))
+      {
+        this->k = _sdf->Get<double>("k");
+      }
+      else
+      {
+        this->k = 5;
+      }
+      this->z_eq = 0.015;
+      double g = 9.81;
+      double m = this->model->GetLink("button")->GetInertial()->GetMass();
+      this->z0 = this->z_eq + (m * g) / this->k;
 
       // Listen to the update event. This event is broadcast every
       // simulation iteration.
@@ -29,8 +44,11 @@ namespace gazebo
     // Called by the world update start event
     public: void OnUpdate(const common::UpdateInfo & /*_info*/)
     {
-      // Apply a force of 2 on the axis 0
-      this->model->GetJoint("glider")->SetForce(0, 2);
+      gazebo::math::Vector3 p = this->model->GetLink("button")->GetRelativePose().pos;
+      double force = - this->k * (p.z - this->z0);
+
+      // Apply a force of on the axis 0
+      this->model->GetJoint("glider")->SetForce(0, force);
     }
   };
 
